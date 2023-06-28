aae6808658687365715577d7fe1f3aa92908c693
Aidan McQuillan <AidanMcQuillan917@outlook.com>
2023-06-27 20:33:58 -0400

    Add project files.

---
+++ Block-Beasts-Library/Trainer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block_Beasts_Library
+{
+    // This class is for the player. The character parent class will be for the Beasts (weapons). This class is strictly just for the storyline of my app.
+    public class Trainer
+    {
+
+        // FIELDS - Funny
+        // I am not using any business rules 
+
+        // PROPS - People
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+
+        // CTOR - Collect
+        public Trainer (string name, int age, string gender) // Params must be closer case so we can assign the props to the params
+        {
+            Name = Name;
+            Age = Age;
+            Gender = Gender; 
+        }
+
+        // METHODS - Monkeys
+
+        public override string ToString()
+        {
+            return $" {Name} - {Age} - {Gender}" ;
+        }
+
+
+    }
+}      
---
+++ Block-Beasts/Program.cs
@@ -0,0 +1,32 @@
+﻿using Block_Beasts;
+using Block_Beasts_Library;
+
+// I've decided to make my own solution while I only reference the centriq code. This will help me better understand the concepts and learn on my own
+
+namespace Block_Beasts
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+
+            Console.WriteLine("...");
+            Thread.Sleep(500);
+            Console.WriteLine("\n... ...\n");
+            Thread.Sleep(1000);
+            Console.WriteLine("\n... ... ...\n");
+            Thread.Sleep(1500);
+            Console.WriteLine("Oh, you're finally awake.\nYou were so nervous you passed out!");
+
+
+        Trainer player = new Trainer("Name",18,"Male");
+
+            Console.WriteLine(player);
+
+
+
+
+
+        }
+    }
+}
