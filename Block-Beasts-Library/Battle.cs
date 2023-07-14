4282b0f0e9ba112517af33410725fcded04bec12
Aidan McQuillan <AidanMcQuillan917@outlook.com>
2023-07-13 20:45:16 -0400

    I created a X testing block for my project.

--- Block-Beasts-Library/Battle.cs
+++ Block-Beasts-Library/Battle.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Block_Beasts_Library.Talents;
 
 namespace Block_Beasts_Library
 {
     public class Battle
     {
         //keep track of average blows it takes to take down a beast
+        static int DodgeChance()
+        {
+            Random DodgeRoll = new Random();
+            int Dodge = DodgeRoll.Next(1, 101);
+
+            if (Dodge < 51)  
+            {
+                // Add the dodge
+                Console.WriteLine("You dodged the attack!");
+            }
+            else if (Dodge < 101)  
+            {
+                 //Add the dodge
+                Console.WriteLine("Your beast is Strong!\nIncreased damage buff, oh yeah!");
+            }
+            return Dodge;
+        }
     }
 }
--- Block-Beasts-Library/Beast.cs
+++ Block-Beasts-Library/Beast.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using static Block_Beasts_Library.Talents;
 
 namespace Block_Beasts_Library
 {
@@ -17,8 +19,8 @@ namespace Block_Beasts_Library
         public int MaxHealth { get; set; }
         public string Type { get; set; }
         public int CriticalHitChance { get; set; }
-        public string HiddenTalent { get; set; }
-        public decimal Damage { get; set; }
+        public Talent HiddenTalent { get; set; }
+        public int Damage { get; set; }
         public int Dodge { get; set; }
         public int Miss { get; set; }
 
@@ -27,7 +29,7 @@ namespace Block_Beasts_Library
 
 
         // CTOR - Collect
-        public Beast(string name, int health, int maxHealth, int criticalHitChance, string hiddenTalent, decimal damage, int dodge, int miss, string description)
+        public Beast(string name, int health, int maxHealth, int criticalHitChance, int damage, int dodge, int miss, string description)
         {
             /*
             --------TALENTS----------
@@ -44,8 +46,8 @@ namespace Block_Beasts_Library
             health = maxHealth;
             //Type = type; // no context yet
             CriticalHitChance = criticalHitChance; // 10 - 20%
-            HiddenTalent = hiddenTalent; // Rolled from the talent enums and each one has an effect 
-            Damage = damage; // Base damage 3-5
+            Damage = this.CalcDamage(); // Base damage 3-5
+            HiddenTalent = this.RandomTalent(); // Rolled from the talent enums and each one has an effect
             Dodge = dodge; // 10-20% base stat
             Miss = miss; // 10-20% base stat 
             Description = description;
@@ -58,14 +60,119 @@ namespace Block_Beasts_Library
             return $"---Check out these stats!---" +
                 $"\nName: {Name}" +
                 $"\nDescription: {Description}" +
-                //$"\nElemental Type: {Type}" +
-                $"\nHidden Talent: {HiddenTalent}" +
+                $"\nHidden Talent: {HiddenTalent} " +
+                $"{(this.HiddenTalent == Talent.Weak ? "\nYour beast is Weak!\nYour frail beast won't do as much damage to others." : "")}" +
                 $"\nMax Health: {MaxHealth}/25" +
                 $"\nDamage Output: {Damage}" +
                 $"\nCritical Hit Chance: {CriticalHitChance}" +
                 $"\nDodge Chance: {Dodge}" +
                 $"\nMiss Chance: {Miss}";
         }
+
+        public int CalcDamage()
+        {
+             
+
+            Random Damage = new Random();
+            int damageAmount = Damage.Next(3, 5);
+
+            
+            
+            return damageAmount; 
+            
+        } // Damage
+        public int CalcMiss()
+        { 
+
+
+            Random Damage = new Random();
+            int damageAmount = Damage.Next(1, 11);
+
+
+            return damageAmount;
+
+        } // Miss
+        public int CalcDodge()
+        {
+
+
+            Random Damage = new Random();
+            int damageAmount = Damage.Next(1, 11);
+
+
+            return damageAmount;
+
+        } // Dodge
+        public int CalcCritical()
+        {
+
+
+            Random Damage = new Random();
+            int damageAmount = Damage.Next(1, 11);
+
+
+            return damageAmount;
+
+        } // Critical 
+        public Talent RandomTalent()
+        {
+
+            
+
+            Talent selectedTalent = Talent.boring;
+
+            Array talents = Enum.GetValues(typeof(Talent));
+            //Random rngTalent = new Random();
+            //Talent randomTalent = (Talent)talents.GetValue(rngTalent.Next(talents.Length));
+
+
+
+
+            Random WeighedTalent = new Random();
+            int TalentPercent = WeighedTalent.Next(1, 101);
+
+            if (TalentPercent < 16) // Numbers 1..10 ( A -> 15% )
+            {
+                selectedTalent = Talent.Timid;
+                Console.WriteLine("Your beast is Timid!\nIt has a better chance at dodging.");
+            }
+            else if (TalentPercent < 31) // Numbers 11..40 ( B -> 15 % )
+            {
+                selectedTalent = Talent.strong;
+                Console.WriteLine("Your beast is Strong!\nIncreased damage buff, oh yeah!");
+            }
+            else if (TalentPercent < 46) // Numbers 41..100 ( C -> 15 % ) 
+            {
+                selectedTalent = Talent.Clumsy;
+                Console.WriteLine("Your beast is Clumsy!\nIncreased Miss chance when in combat :( ");
+            }
+            else if (TalentPercent < 61) // Numbers 41..100 ( C -> 15 % ) 
+            {
+                selectedTalent = Talent.Weak;
+
+                this.Damage -= 1 ;
+            }
+            else if (TalentPercent < 101) // Numbers 41..100 ( C -> 40 % ) 
+            {
+                selectedTalent = Talent.boring;
+                Console.WriteLine("Your beast is Boring!\nYour beast has no change, boo hoo");
+            }
+
+             
+
+
+            return selectedTalent;
+
+
+
+
+
+        }// Talent
+
+       // public void StatChanges()
+        
+
+
 
     }
 }
