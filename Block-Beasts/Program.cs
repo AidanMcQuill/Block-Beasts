9aaea138792fd0aafaa690601d058b79c4298aa3
Aidan McQuillan <AidanMcQuillan917@outlook.com>
2023-07-17 02:08:24 -0400

    Long sessions but everything is working great besides a few more issues and some general customization

--- Block-Beasts/Program.cs
+++ Block-Beasts/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Block_Beasts;
 using Block_Beasts_Library;
 using System;
+using System.Diagnostics;
 using System.Reflection;
 using System.Threading;
+using System.Threading.Channels;
 
 // I've decided to make my own solution while I only reference the centriq code. This will help me better understand the concepts and learn on my own
 // TODO create 5 unit tests for my app.
@@ -13,45 +15,66 @@ namespace Block_Beasts
     {
 
         static void Main(string[] args)
-        //TODO make the player randomly go up against 3 random trainers 
-        //TODO general coloring and stylization
+        //TODO Add fonts if I have time 
         {
             #region Intro 
+            // TITLE SCREEN 
+
+
+            // ASKI art 
+
+            Console.WriteLine(@"
+______ _            _     ______                _       
+| ___ \ |          | |    | ___ \              | |      
+| |_/ / | ___   ___| | __ | |_/ / ___  __ _ ___| |_ ___ 
+| ___ \ |/ _ \ / __| |/ / | ___ \/ _ \/ _` / __| __/ __|
+| |_/ / | (_) | (__|   <  | |_/ /  __/ (_| \__ \ |_\__ \
+\____/|_|\___/ \___|_|\_\ \____/ \___|\__,_|___/\__|___/
+                                                        
+                                                        
+");
+
+            Console.WriteLine("Test your luck and bond with your best beast!   Keep playing to see every beast combination!\n");
+
+            //END OF TITLE SCREEN 
+            Console.Write("Press space to continue\n");
+            Console.ReadKey();
+            Console.Clear();
+
             Console.WriteLine("Hey...\n");
 
             Console.WriteLine("Hey wake up...\n");
 
             Console.WriteLine("HEY!\n");
 
-            Console.WriteLine("Oh, you're finally awake.\nYou were so nervous you passed out!");
+            Console.WriteLine("Oh, you're finally awake... You were so nervous you passed out!\n");
             // Key press to continue
-            Console.WriteLine("You can't remember where you're at? can't you hear the cheers and see all the lights?");
-            Console.WriteLine("You're a competitor in the BLOCK BEAST BATTLE-THON!\n");
-
-            Console.WriteLine("Press space to continue");
-            Console.ReadKey();
-            Console.Clear();
-
+            Console.WriteLine("You can't remember where you're at? You're a competitor in the BLOCK BEAST BATTLE-THON!\n");
+            Console.WriteLine("I'm the referee at this event, let me get you started.");
+            //SPLITER 
+            Console.WriteLine("\n====================================================================================== \n");
             Console.WriteLine("Don't tell me you're too nervous to remember your name?\nTell me, What was it again?\n");
             // laying out the fields to make the props for "player" object
+            Console.Write("NAME: ");
             string TrainerName = Console.ReadLine();
-            Console.WriteLine("\nPerfect, and your age?\n");
-            int TrainerAge = int.Parse(Console.ReadLine());
-            Console.WriteLine($"\nAnd finally, what is your gender?\n");
-            string TrainerGender = Console.ReadLine();
-            Console.WriteLine("\n\nTake this BlockBOX, It's a device to tell you all about this world and it's lifeforms, including yourself.\n");
+            Console.WriteLine("\nWhat a cool name, Hope you're prepared " + TrainerName + "!\n");
+            Console.Clear();
+            Console.WriteLine("====================================================================================== \n");
+            Console.Write("Take this ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("BeastBOX, ");
+            Console.ResetColor();
+            Console.Write("It's a device that records data all about this world and it's lifeforms, including yourself.\n");
+            Trainer player = new Trainer(TrainerName, 0); // other fields added too much dialogue for no reason. 
 
-            // New Object to capture your info 
-            Trainer player = new Trainer(TrainerName, TrainerAge, TrainerGender);
+            Console.WriteLine("\nBlockBOX: Welcome User. Here is what I've recorded so far:\n" + "NAME: " + player.Name);
 
-            Console.WriteLine("\nBlockBOX: Welcome User. Here is what I've recorded so far:\n\n" + player.ToString());
-
-            Console.WriteLine("\nPress space to continue\n");
+            Console.Write("\nPress space to continue\n");
             Console.ReadKey();
             Console.Clear();
 
-            #endregion 
-
+            #endregion
+            // All below code is commented out. 
             #region Beast Rolls 
             //// 6-29 : My first step will be to make my BlockBeast egg hatching mechanic.
             //// I will call to my class library for characteristics for each beast.
@@ -94,7 +117,7 @@ namespace Block_Beasts
 
             #region Select Beasts
 
-             //Console.WriteLine("\nPlease select your starting beast: (you can switch beasts after each fight) \n");
+            //Console.WriteLine("\nPlease select your starting beast: (you can switch beasts after each fight) \n");
             //bool advance = true;
             //do
             //{
@@ -135,27 +158,31 @@ namespace Block_Beasts
 
 
             #endregion
-
-                //Generate a Trainer Beast
-                Beast TrainerBeast = Beast.GetTrainerBeast();
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("=====  The Beast entering the event with you is: " + TrainerBeast.Name + ".  =====");
-                Console.WriteLine(TrainerBeast.ToString());
-                Console.ResetColor();
-
+            #region Trainer Beast
+            //Generate a Trainer Beast
+            Console.WriteLine("Generating Your Beast: "); ShowSimplePercentage();
+            Beast TrainerBeast = Beast.GetTrainerBeast();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n=====  The Beast entering the event with you is: " + TrainerBeast.Name + ".  =====");
+            Console.WriteLine(TrainerBeast.ToString());
+            Console.ResetColor();
+            #endregion
+            #region Beast and Arena generation
             bool Quit = false;
+            int Score = -1;
             do
             {
-                #region Beast and Arena generation
 
+               
 
                 //Generates Arena
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("\n" + GetArena());
                 Console.ResetColor();
                 //Generate a Enemy Beast  
+                Console.WriteLine("Generating Enemy Beast: "); ShowSimplePercentage();
                 Beast EnemyBeast = Beast.GetEnemyBeast();
-                Console.WriteLine("=====  In this arena you are fighting a " + EnemyBeast.Name + "! Stay Alert Trainer!  =====");
+                Console.WriteLine("\n=====  In this arena you are fighting a " + EnemyBeast.Name + "! Stay Alert Trainer!  =====");
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine(EnemyBeast.ToString());
                 Console.ResetColor();
@@ -164,16 +191,19 @@ namespace Block_Beasts
                 #endregion
 
                 #region Menu Loop 
- 
 
-                TrainerBeast.score = 0;
+
+
+                Score += 1;// This happens every time a new beast is generated (meaning you won a battle).
 
 
                 //Gameplay Loop after everything is setup
+                bool BlockBoot = false;
                 bool reload = false;
                 do
                 {
-                    Console.WriteLine("\nTime to act " + TrainerName + ":\n" +
+
+                    Console.WriteLine("\nTime to act " + player.Name + ":\n" +
                         "1) Attack\n" +
                         "2) BeastBOX\n" +
                         "3) Exit");
@@ -187,57 +217,90 @@ namespace Block_Beasts
                             reload = Battle.BeastBattle(TrainerBeast, EnemyBeast);
                             break;
                         #region BlockBOX 
-                        // TODO add a score option
+                        //TODONE (It works but not fully how I'd like it to) study and make a proper BlockBOX loop for this menu. 
                         case '2':
-                            Console.WriteLine("BeastBOX:\n");
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.Write("BeastBOX:\n");
+                            Console.ResetColor();
                             Console.WriteLine("Player info:\n ");
                             //print player details to the screen
-                            Console.WriteLine(player);
-                            Console.WriteLine("\nHello " + TrainerName + "! please select an option:\n" +
-                        "1) Beast Info\n" +
-                        "2) Enemy info\n" +
-                        "3) Exit back to battle\n");
-
-                            char BoxChoice = Console.ReadKey(true).KeyChar;
-                            switch (BoxChoice)
+                            Console.WriteLine("Name: " + player.Name);
+                            Console.WriteLine("Current Beast: " + TrainerBeast.Name);
+                            bool BoxLoop = true;
+                            while (BoxLoop)
                             {
-                                case '1':
-                                    Console.WriteLine("Trainer Beast Info:\n");
-                                    Console.WriteLine(TrainerBeast.ToString() + "\n");
 
-                                    break;
-                                case '2':
-                                    Console.WriteLine("Enemy Beast Info:\n");
-                                    Console.WriteLine(EnemyBeast.ToString() + "\n");
-                                    break;
-                                case '3':
-                                    // Exits BlockBOX
-                                    break;
-                                default:
+                                Console.WriteLine("\nHello " + player.Name + "! please select an option:\n" +
+                            "1) Beast Info\n" +
+                            "2) Enemy info\n" +
+                            "3) Score\n" +
+                            "4) Exit back to battle\n");
 
-                                    break;
+                                char BoxChoice = Console.ReadKey(true).KeyChar;
+                                switch (BoxChoice)
+                                {
+                                    case '1':
+                                        Console.WriteLine("Trainer Beast Info:\n");
+                                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                                        Console.WriteLine(TrainerBeast.ToString() + "\n");
+                                        Console.ResetColor();
+                                        
+
+                                        break;
+                                    case '2':
+                                        Console.WriteLine("Enemy Beast Info:\n");
+                                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                                        Console.WriteLine(EnemyBeast.ToString() + "\n");
+                                        Console.ResetColor();
+                                       
+                                        break;
+                                    case '3':
+                                        Console.WriteLine("\nBattles Won: " + Score); ;
+                                       
+                                        break;
+                                    case '4':
+                                        // Exits BlockBOX
+                                        
+                                        break;
+                                    default:
+                                        Console.WriteLine("Choose a proper input trainer!");
+                                        
+                                        break;
+
+                                }
+                                break;
                             }
                             break;
-                            reload = true;
-                            break;
-                        #endregion
+
+
+
+                        #endregion // end of block box 
                         case '3':
                             Console.WriteLine("Exiting App... ");
                             //print player details to the screen
                             Quit = true;
                             break;
 
+
                         default:
                             Console.WriteLine("Now isn't the time for that trainer, select an option! ");
                             break;
+
                     }//end switch
 
+                    // Perma Text for menu 
+                    Console.WriteLine("\n" + TrainerBeast.Name + " VS " + EnemyBeast.Name + "\n");
+
+
                     //HEALTH CHECK
+
                     if (TrainerBeast.Health <= 0)
                     {
-                        Console.WriteLine("You have lost the battle...");
+                        Console.WriteLine("\nYou have lost the battle...");
+                        Console.WriteLine("reload and keep playing to see every beast combination!");
                         Quit = true;
                     }
+
 
 
                 } while (!reload && !Quit);
@@ -248,8 +311,8 @@ namespace Block_Beasts
 
             #region Run Results 
 
-            Console.WriteLine("You won " + TrainerBeast.score +
-              " battle" + (TrainerBeast.score == 1 ? "." : "s."));
+            Console.WriteLine("You won " + TrainerBeast.Score +
+              " battle" + (TrainerBeast.Score == 1 ? "." : "s."));
 
             #endregion
 
@@ -271,11 +334,28 @@ namespace Block_Beasts
             string Arena = Arenas[index];
             return Arena;
 
-            #endregion
 
         }
+        static void ShowSimplePercentage()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.Write($"\rProgress: {i}%   ");
+                Thread.Sleep(25);
+            }
+
+            Console.Write("\rDone!          ");
+        }
+
+
+
     }
+    #endregion
+
+
+
 }
+
 
 
 
